7a5be99a55da4f6c5a5da01cbb2482385ffcdf8c
Gusti Muhammad Adzaky <zakygustu@gmail.com>
2020-04-08 12:43:08 +0700

    Add files via upload

---
+++ Inheritance #30 - Java - Bahasa Indonesia/src/belajaroop/Circle.java
@@ -0,0 +1,45 @@
+package belajaroop;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle(){
+
+    }
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color){
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setColor (String color){
+        this.color = color;
+    }
+
+    public double getArea(){
+        return Math.PI * this.radius * this.radius;
+    }
+
+    public String thString(){
+        return "Luasnya Adalah " + this.getArea();
+    }
+
+
+}
